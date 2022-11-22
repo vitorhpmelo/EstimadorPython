1718020a879fef62f3a13cbe40b59aeb79a69dd3
Vítor Melo <vitorhpmelo@pop-os.localdomain>
2022-11-21 22:33:09 -0300

    coments on the functions

--- networkstruc.py
+++ networkstruc.py
@@ -3,31 +3,48 @@ from classes import *
 import numpy as np
 
 def creat_bar(dfDBAR):  
+    """
+    Function to read the information about the network in the data frame and put it into the bar struture
+    @param: dfDBAR - data frame with the informations about the network
+    @return: vector of instances bar class, with the information about the netowrk buses
+    @return: i - number of buses
+    @return: pv - index of the pv buses
+    @return: pq - index of the pq buses
+    @return: ind_id - dict to convert the name of the bus to its index
+    """
     bars=[]
     i=0
     nvar=0
     pv=[]
     pq=[]
     ind_id={}
-    for idx, row in dfDBAR.iterrows():
-        item=bar(int(row["id"]),int(row["type"]),i)
+    for idx, row in dfDBAR.iterrows():#reads each line of the data frame
+        item=bar(int(row["id"]),int(row["type"]),i) 
         item.V=row.V
-        item.teta=row.teta*np.pi/180
-        item.Pg=row["Pg"]/100
-        item.Qg=row["Qg"]/100
-        item.Pd=row["Pd"]/100
-        item.Qd=row["Qd"]/100
-        item.Bs=row["Bs"]/100
+        item.teta=row.teta*np.pi/180 # converts the angle to radians
+        item.Pg=row["Pg"]/100#converts the power from MW to p.u.
+        item.Qg=row["Qg"]/100#converts the power from MW to p.u.
+        item.Pd=row["Pd"]/100#converts the power from MW to p.u.
+        item.Qd=row["Qd"]/100#converts the power from MW to p.u.
+        item.Bs=row["Bs"]/100#converts the power from MW to p.u.
         if int(row["type"])==1:
-            pv.append(i)
+            pv.append(i)#create the list of PV buses
         elif int(row["type"])==2:
-            pq.append(i)
+            pq.append(i)#create the list of PQ buses
         bars.append(item)
-        ind_id[int(row["id"])]=i
+        ind_id[int(row["id"])]=i # create the dictionary relating the name of the bus to it index
         i=i+1
     return bars,i,pv,pq,ind_id
 
 def create_bran(dfDBRAN,ind_i):
+    """
+    Function to read the information in the data frame dfDBRAN and put it in the ram dictionary, that is composed by instances of the
+    branch class. This ditc contains all the information about the network branches.
+    @param: dfDBRAN - Data Frame with the information about the buses
+    @param: ind_i - dict to translate the name of the bus to it index
+    @return ram - list of instances of branch class with the information about the network branches
+    @return i - number of branches 
+    """
     ram={}
     i=0
     for id, row in dfDBRAN.iterrows():
@@ -35,23 +52,29 @@ def create_bran(dfDBRAN,ind_i):
         item=branch(int(row["id"]),ind_i[int(row["de"])],ind_i[int(row["para"])],int(row["type"]),i)
         item.x=row["x"]
         item.r=row["r"]
-        item.bsh=complex(0,row["bsh"]/2)
+        item.bsh=complex(0,row["bsh"]/2) #divides the shunt suceptance by two
         item.tap=row["tap"]
-        item.cykm()
-        item.twoPortCircuit()
+        item.cykm()#calculates the ykm
+        item.twoPortCircuit()#creates the two port circuit
         ram[key]=item    
         i+=1
     return ram,i
 
 def create_graph(bars,ram):
+    """
+    Creates the network graph usinf the information of the bars list and the ram dic.
+    @param bars - list of instances of the class bar with the information about the network buses
+    @param ram - list of instances of branch class with the information about the network branches
+    @return graph - list of instances of the class node, that form the network graph
+    """
     graph=[]
     for item in bars:
         node=node_graph(item.i,item)
         if abs(item.Bs)>1e-8:
-            node.FlagBS=1
+            node.FlagBS=1#informs if exists a capacitor bank or a reactor
             node.Bs=item.Bs
         graph.append(node)
-    for key,item in ram.items():
+    for key,item in ram.items(): #save the adjacent buses in the node and the rams connected to it
         k=int(key.split("-")[0])
         m=int(key.split("-")[1])
         graph[k].adjk.update({key:item})
--- readfiles.py
+++ readfiles.py
@@ -3,20 +3,27 @@ import pandas as pd
 import numpy as np
 
 def read_files(sys):
-    try:
+    """
+    This function performs the reading of the files with the information about the Power System.
+    @param: sys - string with the name of the system
+    @return dfDBAR - Data Frame with the information about the system's buses
+    @return dfDBRAN - Data Frame with the information about the system's branches
+    @return dfDMED - Data Frame with the information about the system measurements
+    """
+    try: # if the DBAR exists the program reads it, if not it stops. This file is mandatory 
         dfDBAR=pd.read_csv(sys+"/DBAR.csv",header=None,dtype={0:np.int64,1:np.int64})
         dfDBAR.columns=["id","type","V","teta","Pg","Qg","Pd","Qd","Bs"]
     except:
         print("Error while reading DBAR file")
         exit(1)
 
-    try:
+    try: # if the DBRAN exists the program reads it, if not it stops. This file is mandatory
         dfDBRAN=pd.read_csv(sys+"/DBRAN.csv",header=None,dtype={0:np.int64,1:np.int64,2:np.int64,3:np.int64})
         dfDBRAN.columns=["id","type","de","para","r","x","bsh","tap"]
     except:
         print("Error while reading DBRAN file")
         exit(1)
-    try:
+    try: # if the DMED exists the program reads it, this file is not mandatory for power flow 
         dfDMED=pd.read_csv(sys+"/DMED.csv",header=None)
         dfDMED.columns=["type","de","para","zmed","prec"]
     except:
@@ -26,13 +33,25 @@ def read_files(sys):
 
 
 def prt_state(graph):
+    """
+    Function to print in the scream the value of the state variables, in the network's graph
+    @param: graph Graph structure with the information about the network
+    """
     for no in graph:
         s="Barra: {:d} | V : {:f} | teta : {:f}".format(no.bar.id,no.V,no.teta*180/np.pi)
         print(s)
 
 
 def save_DMED_fp(graph,ram,sys):
-    medidas=[]
+    """
+    Function to save the file with all measurements possible, from a load flow simulation. 
+    It use the graph of the network to calculate every possible measurement and save it in a file called 
+    DMED_fp.csv into the system's folder.
+    @param: graph Graph structure with the information about the network
+    @param: ram dictionary with the information about the network branches
+    @sys: string with the system folder's name
+    """
+    medidas=[] 
     Pinj=[]
     Qinj=[]
     Vmod=[]
@@ -40,6 +59,7 @@ def save_DMED_fp(graph,ram,sys):
     Pmk=[]
     Qkm=[]
     Qmk=[]
+    #calculates the Power Inejection (Reactive and Active)
     for no in graph:
         linha=[0,no.bar.id,-1,no.P(graph),1]
         Pinj.append(linha)
@@ -48,11 +68,14 @@ def save_DMED_fp(graph,ram,sys):
         linha=[4,no.bar.id,-1,no.V,1]
         Vmod.append(linha)
 
+    #calculates the flows in the branches
     for key,r in ram.items():
+        #calculate from k to m
         linha=[2,graph[r.de].bar.id,graph[r.para].bar.id,r.Pf(graph,0),1.0]
         linha2=[3,graph[r.de].bar.id,graph[r.para].bar.id,r.Qf(graph,0),1.0]
         Pkm.append(linha)
         Qkm.append(linha2)
+        #calculate from m to k
         linha=[2,graph[r.para].bar.id,graph[r.de].bar.id,r.Pf(graph,1),1.0]
         linha2=[3,graph[r.para].bar.id,graph[r.de].bar.id,r.Qf(graph,1),1.0]
         Pmk.append(linha)
