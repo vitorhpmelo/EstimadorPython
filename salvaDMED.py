09ef36ae831a366ca046082e3434ad4d2988406e
Vítor Melo <vitorhpmelo@pop-os.localdomain>
2023-09-15 16:41:53 -0300

    AAAA

--- salvaDMED.py
+++ salvaDMED.py
@@ -16,7 +16,7 @@ import scipy.sparse.linalg as sliang
 
 #%% Lê arquivos e constroi a estrutura da rede
 
-sys="IEEE118_rakp2009"
+sys="IEEE14_rakp2009"
 
 
 dfDBAR,dfDBRAN,dfDMED,dfDFACTS=read_files(sys)
